4c8054a01c87b418b40283bfb08627772d828d7f
Avishka Umesh <umesh.avishka@gmail.com>
2023-09-06 23:56:11 +0530

    navbar mobile button added

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { FiAlignJustify } from "react-icons/fi";
 import { RiMoonFill, RiSunFill } from "react-icons/ri";
 
 const Navbar = (props) => {
+  const { onOpenSidenav } = props;
   const [darkmode, setDarkmode] = React.useState(false);
 
   return (
     <nav className="sticky top-4 z-40 flex flex-row flex-wrap items-center justify-between rounded-xl bg-white/10 p-2 backdrop-blur-xl dark:bg-[#0b14374d]">
-      <div className="ml-[6px]"></div>
+      <div className="ml-[6px]">
+        <span
+          className="flex cursor-pointer text-xl text-gray-600 dark:text-white xl:hidden"
+          onClick={onOpenSidenav}
+        >
+          <FiAlignJustify className="h-5 w-5" />
+        </span>
+      </div>
 
       <div className="relative  rounded-full bg-white px-2 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none ">
         <div
